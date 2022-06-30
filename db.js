80d9d032ec4dd2a016a4169ac7deda8ae4b2a52f
Leandro S <76525924+Lean788@users.noreply.github.com>
2022-06-30 13:58:50 +0200

    Add a .env and DDBB

---
+++ db.js
@@ -0,0 +1,701 @@
+module.exports = [{
+    "id": 1,
+    "name": "Gabie Shepton",
+    "email": "gshepton0@netscape.com",
+    "carBrand": "Lexus",
+    "carModel": "GS",
+    "carYear": 2003
+  }, {
+    "id": 2,
+    "name": "Danni Shelton",
+    "email": "dshelton1@columbia.edu",
+    "carBrand": "Lexus",
+    "carModel": "SC",
+    "carYear": 2002
+  }, {
+    "id": 3,
+    "name": "Merrile Kulic",
+    "email": "mkulic2@ask.com",
+    "carBrand": "Infiniti",
+    "carModel": "QX",
+    "carYear": 2011
+  }, {
+    "id": 4,
+    "name": "Joelie Trousdell",
+    "email": "jtrousdell3@domainmarket.com",
+    "carBrand": "Honda",
+    "carModel": "CR-V",
+    "carYear": 1999
+  }, {
+    "id": 5,
+    "name": "Britni Edscer",
+    "email": "bedscer4@uiuc.edu",
+    "carBrand": "Mitsubishi",
+    "carModel": "Expo LRV",
+    "carYear": 1996
+  }, {
+    "id": 6,
+    "name": "Paco Phinnis",
+    "email": "pphinnis5@123-reg.co.uk",
+    "carBrand": "Subaru",
+    "carModel": "XT",
+    "carYear": 1986
+  }, {
+    "id": 7,
+    "name": "Ali Eltun",
+    "email": "aeltun6@huffingtonpost.com",
+    "carBrand": "Eagle",
+    "carModel": "Talon",
+    "carYear": 1996
+  }, {
+    "id": 8,
+    "name": "Penny Agiolfinger",
+    "email": "pagiolfinger7@pinterest.com",
+    "carBrand": "Hummer",
+    "carModel": "H3",
+    "carYear": 2006
+  }, {
+    "id": 9,
+    "name": "Sherline Landers",
+    "email": "slanders8@miibeian.gov.cn",
+    "carBrand": "Cadillac",
+    "carModel": "Seville",
+    "carYear": 1999
+  }, {
+    "id": 10,
+    "name": "Matti Kos",
+    "email": "mkos9@yelp.com",
+    "carBrand": "Volkswagen",
+    "carModel": "Jetta",
+    "carYear": 2007
+  }, {
+    "id": 11,
+    "name": "Tabb Griniov",
+    "email": "tgriniova@cornell.edu",
+    "carBrand": "Dodge",
+    "carModel": "Dakota Club",
+    "carYear": 2005
+  }, {
+    "id": 12,
+    "name": "Jeanie Brookfield",
+    "email": "jbrookfieldb@state.gov",
+    "carBrand": "Dodge",
+    "carModel": "Ram 3500",
+    "carYear": 2008
+  }, {
+    "id": 13,
+    "name": "Sinclare Chorlton",
+    "email": "schorltonc@list-manage.com",
+    "carBrand": "Saab",
+    "carModel": "9-7X",
+    "carYear": 2008
+  }, {
+    "id": 14,
+    "name": "Tally Dunnan",
+    "email": "tdunnand@stumbleupon.com",
+    "carBrand": "Honda",
+    "carModel": "Insight",
+    "carYear": 2003
+  }, {
+    "id": 15,
+    "name": "Lizette Beldom",
+    "email": "lbeldome@lycos.com",
+    "carBrand": "BMW",
+    "carModel": "X3",
+    "carYear": 2012
+  }, {
+    "id": 16,
+    "name": "Lolly Aggs",
+    "email": "laggsf@home.pl",
+    "carBrand": "Lexus",
+    "carModel": "GS",
+    "carYear": 2004
+  }, {
+    "id": 17,
+    "name": "Kendra Kardos",
+    "email": "kkardosg@yellowbook.com",
+    "carBrand": "Volvo",
+    "carModel": "S90",
+    "carYear": 1997
+  }, {
+    "id": 18,
+    "name": "Parker Rassmann",
+    "email": "prassmannh@goodreads.com",
+    "carBrand": "Mercury",
+    "carModel": "Sable",
+    "carYear": 1996
+  }, {
+    "id": 19,
+    "name": "Anet Levermore",
+    "email": "alevermorei@skype.com",
+    "carBrand": "Chevrolet",
+    "carModel": "Cobalt",
+    "carYear": 2008
+  }, {
+    "id": 20,
+    "name": "Marissa Heningham",
+    "email": "mheninghamj@redcross.org",
+    "carBrand": "Jeep",
+    "carModel": "Compass",
+    "carYear": 2012
+  }, {
+    "id": 21,
+    "name": "Bettye Cookley",
+    "email": "bcookleyk@loc.gov",
+    "carBrand": "Geo",
+    "carModel": "Tracker",
+    "carYear": 1995
+  }, {
+    "id": 22,
+    "name": "Giselbert Denziloe",
+    "email": "gdenziloel@cbsnews.com",
+    "carBrand": "Ford",
+    "carModel": "Fiesta",
+    "carYear": 2013
+  }, {
+    "id": 23,
+    "name": "Mano Chipperfield",
+    "email": "mchipperfieldm@lycos.com",
+    "carBrand": "Volvo",
+    "carModel": "V70",
+    "carYear": 2009
+  }, {
+    "id": 24,
+    "name": "Hobey Dugmore",
+    "email": "hdugmoren@shinystat.com",
+    "carBrand": "Mazda",
+    "carModel": "Miata MX-5",
+    "carYear": 1999
+  }, {
+    "id": 25,
+    "name": "Lia Hussell",
+    "email": "lhussello@wp.com",
+    "carBrand": "Mazda",
+    "carModel": "B-Series",
+    "carYear": 2000
+  }, {
+    "id": 26,
+    "name": "Pieter Crang",
+    "email": "pcrangp@slideshare.net",
+    "carBrand": "Volkswagen",
+    "carModel": "Jetta",
+    "carYear": 2005
+  }, {
+    "id": 27,
+    "name": "Doloritas Androletti",
+    "email": "dandrolettiq@narod.ru",
+    "carBrand": "Chrysler",
+    "carModel": "New Yorker",
+    "carYear": 1994
+  }, {
+    "id": 28,
+    "name": "Katrinka Raubenheimer",
+    "email": "kraubenheimerr@bloglovin.com",
+    "carBrand": "Ford",
+    "carModel": "Mustang",
+    "carYear": 2013
+  }, {
+    "id": 29,
+    "name": "Salli Franzettoini",
+    "email": "sfranzettoinis@jugem.jp",
+    "carBrand": "Nissan",
+    "carModel": "Quest",
+    "carYear": 2000
+  }, {
+    "id": 30,
+    "name": "Blondie McGavin",
+    "email": "bmcgavint@nhs.uk",
+    "carBrand": "Cadillac",
+    "carModel": "Fleetwood",
+    "carYear": 1995
+  }, {
+    "id": 31,
+    "name": "Cleon Ludlem",
+    "email": "cludlemu@narod.ru",
+    "carBrand": "BMW",
+    "carModel": "X5",
+    "carYear": 2013
+  }, {
+    "id": 32,
+    "name": "Salomo Dowderswell",
+    "email": "sdowderswellv@phoca.cz",
+    "carBrand": "Land Rover",
+    "carModel": "LR3",
+    "carYear": 2009
+  }, {
+    "id": 33,
+    "name": "Windham Garment",
+    "email": "wgarmentw@sbwire.com",
+    "carBrand": "GMC",
+    "carModel": "2500",
+    "carYear": 1996
+  }, {
+    "id": 34,
+    "name": "Kessiah Hartop",
+    "email": "khartopx@ehow.com",
+    "carBrand": "Ford",
+    "carModel": "Courier",
+    "carYear": 1988
+  }, {
+    "id": 35,
+    "name": "Hoebart Werlock",
+    "email": "hwerlocky@twitpic.com",
+    "carBrand": "Dodge",
+    "carModel": "Charger",
+    "carYear": 1969
+  }, {
+    "id": 36,
+    "name": "Alverta Trussman",
+    "email": "atrussmanz@ucsd.edu",
+    "carBrand": "Ford",
+    "carModel": "Tempo",
+    "carYear": 1992
+  }, {
+    "id": 37,
+    "name": "Tobie Conachie",
+    "email": "tconachie10@devhub.com",
+    "carBrand": "Saturn",
+    "carModel": "Ion",
+    "carYear": 2007
+  }, {
+    "id": 38,
+    "name": "Lezlie Acutt",
+    "email": "lacutt11@wufoo.com",
+    "carBrand": "Ford",
+    "carModel": "F150",
+    "carYear": 2006
+  }, {
+    "id": 39,
+    "name": "Annice Sinncock",
+    "email": "asinncock12@altervista.org",
+    "carBrand": "Volkswagen",
+    "carModel": "Jetta",
+    "carYear": 1996
+  }, {
+    "id": 40,
+    "name": "Imogene Delbergue",
+    "email": "idelbergue13@prweb.com",
+    "carBrand": "Lincoln",
+    "carModel": "MKS",
+    "carYear": 2011
+  }, {
+    "id": 41,
+    "name": "Chrissie Treadaway",
+    "email": "ctreadaway14@godaddy.com",
+    "carBrand": "Mercury",
+    "carModel": "Grand Marquis",
+    "carYear": 1997
+  }, {
+    "id": 42,
+    "name": "Averil Bracknall",
+    "email": "abracknall15@wunderground.com",
+    "carBrand": "Lexus",
+    "carModel": "GS",
+    "carYear": 2010
+  }, {
+    "id": 43,
+    "name": "Evin Johanning",
+    "email": "ejohanning16@buzzfeed.com",
+    "carBrand": "Mitsubishi",
+    "carModel": "Outlander",
+    "carYear": 2006
+  }, {
+    "id": 44,
+    "name": "Cordie Cathel",
+    "email": "ccathel17@dailymotion.com",
+    "carBrand": "Kia",
+    "carModel": "Optima",
+    "carYear": 2002
+  }, {
+    "id": 45,
+    "name": "Charmian Tunder",
+    "email": "ctunder18@wordpress.com",
+    "carBrand": "Infiniti",
+    "carModel": "G35",
+    "carYear": 2007
+  }, {
+    "id": 46,
+    "name": "Rozina Snuggs",
+    "email": "rsnuggs19@ed.gov",
+    "carBrand": "Dodge",
+    "carModel": "D350 Club",
+    "carYear": 1993
+  }, {
+    "id": 47,
+    "name": "Hynda Snuggs",
+    "email": "hsnuggs1a@nhs.uk",
+    "carBrand": "Ford",
+    "carModel": "Econoline E250",
+    "carYear": 2002
+  }, {
+    "id": 48,
+    "name": "Karole Urien",
+    "email": "kurien1b@hud.gov",
+    "carBrand": "GMC",
+    "carModel": "Rally Wagon 1500",
+    "carYear": 1992
+  }, {
+    "id": 49,
+    "name": "Anneliese Bastian",
+    "email": "abastian1c@harvard.edu",
+    "carBrand": "Mercedes-Benz",
+    "carModel": "SL-Class",
+    "carYear": 2012
+  }, {
+    "id": 50,
+    "name": "Danyette Andrzej",
+    "email": "dandrzej1d@lulu.com",
+    "carBrand": "BMW",
+    "carModel": "5 Series",
+    "carYear": 2010
+  }, {
+    "id": 51,
+    "name": "Juliana Bissill",
+    "email": "jbissill1e@mashable.com",
+    "carBrand": "GMC",
+    "carModel": "Suburban 2500",
+    "carYear": 1996
+  }, {
+    "id": 52,
+    "name": "Alexei Ramsey",
+    "email": "aramsey1f@sciencedirect.com",
+    "carBrand": "Mercedes-Benz",
+    "carModel": "300SE",
+    "carYear": 1993
+  }, {
+    "id": 53,
+    "name": "Marillin Kuller",
+    "email": "mkuller1g@oaic.gov.au",
+    "carBrand": "Mazda",
+    "carModel": "Miata MX-5",
+    "carYear": 1997
+  }, {
+    "id": 54,
+    "name": "Lizabeth Losel",
+    "email": "llosel1h@tiny.cc",
+    "carBrand": "Cadillac",
+    "carModel": "SRX",
+    "carYear": 2009
+  }, {
+    "id": 55,
+    "name": "Leopold Oland",
+    "email": "loland1i@harvard.edu",
+    "carBrand": "Mitsubishi",
+    "carModel": "Eclipse",
+    "carYear": 2000
+  }, {
+    "id": 56,
+    "name": "Erhart Hamshere",
+    "email": "ehamshere1j@hatena.ne.jp",
+    "carBrand": "Buick",
+    "carModel": "Century",
+    "carYear": 2004
+  }, {
+    "id": 57,
+    "name": "Marj Sherlock",
+    "email": "msherlock1k@rediff.com",
+    "carBrand": "Oldsmobile",
+    "carModel": "Cutlass Supreme",
+    "carYear": 1996
+  }, {
+    "id": 58,
+    "name": "Ingemar Mathes",
+    "email": "imathes1l@sourceforge.net",
+    "carBrand": "Lexus",
+    "carModel": "ES",
+    "carYear": 1993
+  }, {
+    "id": 59,
+    "name": "Filippo Studholme",
+    "email": "fstudholme1m@mediafire.com",
+    "carBrand": "GMC",
+    "carModel": "Jimmy",
+    "carYear": 2001
+  }, {
+    "id": 60,
+    "name": "Ab Wemyss",
+    "email": "awemyss1n@usda.gov",
+    "carBrand": "Plymouth",
+    "carModel": "Colt",
+    "carYear": 1993
+  }, {
+    "id": 61,
+    "name": "Raine Shepley",
+    "email": "rshepley1o@yellowpages.com",
+    "carBrand": "Oldsmobile",
+    "carModel": "Silhouette",
+    "carYear": 2001
+  }, {
+    "id": 62,
+    "name": "Carlye Ferry",
+    "email": "cferry1p@goo.gl",
+    "carBrand": "Toyota",
+    "carModel": "Paseo",
+    "carYear": 1992
+  }, {
+    "id": 63,
+    "name": "Emmy Odams",
+    "email": "eodams1q@discuz.net",
+    "carBrand": "Mercedes-Benz",
+    "carModel": "SLS AMG",
+    "carYear": 2012
+  }, {
+    "id": 64,
+    "name": "Eydie Nibley",
+    "email": "enibley1r@bandcamp.com",
+    "carBrand": "Chevrolet",
+    "carModel": "S10 Blazer",
+    "carYear": 1992
+  }, {
+    "id": 65,
+    "name": "Fredrick Bilson",
+    "email": "fbilson1s@yahoo.com",
+    "carBrand": "Chrysler",
+    "carModel": "Town & Country",
+    "carYear": 1992
+  }, {
+    "id": 66,
+    "name": "Sallee Matthiae",
+    "email": "smatthiae1t@lycos.com",
+    "carBrand": "Dodge",
+    "carModel": "Durango",
+    "carYear": 2001
+  }, {
+    "id": 67,
+    "name": "Blakeley Commin",
+    "email": "bcommin1u@google.fr",
+    "carBrand": "Lexus",
+    "carModel": "LX",
+    "carYear": 2008
+  }, {
+    "id": 68,
+    "name": "Emlyn Maudlin",
+    "email": "emaudlin1v@rambler.ru",
+    "carBrand": "Dodge",
+    "carModel": "Ram 2500 Club",
+    "carYear": 1997
+  }, {
+    "id": 69,
+    "name": "Paulie Galego",
+    "email": "pgalego1w@feedburner.com",
+    "carBrand": "Acura",
+    "carModel": "Legend",
+    "carYear": 1990
+  }, {
+    "id": 70,
+    "name": "Ellissa Kleinzweig",
+    "email": "ekleinzweig1x@who.int",
+    "carBrand": "BMW",
+    "carModel": "7 Series",
+    "carYear": 2005
+  }, {
+    "id": 71,
+    "name": "Addison Lewtey",
+    "email": "alewtey1y@oracle.com",
+    "carBrand": "Jeep",
+    "carModel": "Grand Cherokee",
+    "carYear": 1996
+  }, {
+    "id": 72,
+    "name": "Greg Emnoney",
+    "email": "gemnoney1z@taobao.com",
+    "carBrand": "Audi",
+    "carModel": "A8",
+    "carYear": 1998
+  }, {
+    "id": 73,
+    "name": "Wyatan Leng",
+    "email": "wleng20@taobao.com",
+    "carBrand": "Nissan",
+    "carModel": "Sentra",
+    "carYear": 2006
+  }, {
+    "id": 74,
+    "name": "Waly Barbosa",
+    "email": "wbarbosa21@columbia.edu",
+    "carBrand": "Nissan",
+    "carModel": "Frontier",
+    "carYear": 2011
+  }, {
+    "id": 75,
+    "name": "Luciana McInnes",
+    "email": "lmcinnes22@sohu.com",
+    "carBrand": "Porsche",
+    "carModel": "Boxster",
+    "carYear": 2001
+  }, {
+    "id": 76,
+    "name": "Madelaine Grzegorek",
+    "email": "mgrzegorek23@sogou.com",
+    "carBrand": "Dodge",
+    "carModel": "Dakota Club",
+    "carYear": 2007
+  }, {
+    "id": 77,
+    "name": "Olympie Poynser",
+    "email": "opoynser24@nbcnews.com",
+    "carBrand": "Mercedes-Benz",
+    "carModel": "CLK-Class",
+    "carYear": 2007
+  }, {
+    "id": 78,
+    "name": "Prudi Dacre",
+    "email": "pdacre25@berkeley.edu",
+    "carBrand": "Acura",
+    "carModel": "RL",
+    "carYear": 2010
+  }, {
+    "id": 79,
+    "name": "Lucio Waugh",
+    "email": "lwaugh26@tuttocitta.it",
+    "carBrand": "Buick",
+    "carModel": "Regal",
+    "carYear": 1997
+  }, {
+    "id": 80,
+    "name": "Cosette Proschek",
+    "email": "cproschek27@loc.gov",
+    "carBrand": "Volvo",
+    "carModel": "V70",
+    "carYear": 2003
+  }, {
+    "id": 81,
+    "name": "Portia McMechan",
+    "email": "pmcmechan28@meetup.com",
+    "carBrand": "Suzuki",
+    "carModel": "XL-7",
+    "carYear": 2004
+  }, {
+    "id": 82,
+    "name": "Deeanne Freestone",
+    "email": "dfreestone29@twitpic.com",
+    "carBrand": "Chevrolet",
+    "carModel": "Vega",
+    "carYear": 1971
+  }, {
+    "id": 83,
+    "name": "Christophorus Killoran",
+    "email": "ckilloran2a@mozilla.org",
+    "carBrand": "Volvo",
+    "carModel": "S40",
+    "carYear": 2008
+  }, {
+    "id": 84,
+    "name": "Beck Kondrat",
+    "email": "bkondrat2b@google.co.uk",
+    "carBrand": "GMC",
+    "carModel": "Yukon",
+    "carYear": 2012
+  }, {
+    "id": 85,
+    "name": "Lance Boost",
+    "email": "lboost2c@youtu.be",
+    "carBrand": "Pontiac",
+    "carModel": "Trans Sport",
+    "carYear": 1991
+  }, {
+    "id": 86,
+    "name": "Gregoire Rominov",
+    "email": "grominov2d@flickr.com",
+    "carBrand": "GMC",
+    "carModel": "Yukon",
+    "carYear": 2011
+  }, {
+    "id": 87,
+    "name": "Prince Boise",
+    "email": "pboise2e@amazon.com",
+    "carBrand": "Chevrolet",
+    "carModel": "Venture",
+    "carYear": 1999
+  }, {
+    "id": 88,
+    "name": "Charleen Prium",
+    "email": "cprium2f@amazon.de",
+    "carBrand": "Toyota",
+    "carModel": "Camry",
+    "carYear": 2010
+  }, {
+    "id": 89,
+    "name": "Sayre Yoslowitz",
+    "email": "syoslowitz2g@eventbrite.com",
+    "carBrand": "Infiniti",
+    "carModel": "FX",
+    "carYear": 2010
+  }, {
+    "id": 90,
+    "name": "Eugenia Climar",
+    "email": "eclimar2h@technorati.com",
+    "carBrand": "Mitsubishi",
+    "carModel": "Truck",
+    "carYear": 1995
+  }, {
+    "id": 91,
+    "name": "Thacher Matyatin",
+    "email": "tmatyatin2i@godaddy.com",
+    "carBrand": "Audi",
+    "carModel": "A6",
+    "carYear": 1996
+  }, {
+    "id": 92,
+    "name": "Wainwright Gashion",
+    "email": "wgashion2j@arstechnica.com",
+    "carBrand": "Eagle",
+    "carModel": "Vision",
+    "carYear": 1997
+  }, {
+    "id": 93,
+    "name": "Estell Cuffe",
+    "email": "ecuffe2k@fastcompany.com",
+    "carBrand": "Chevrolet",
+    "carModel": "Equinox",
+    "carYear": 2011
+  }, {
+    "id": 94,
+    "name": "Kitty Mulchrone",
+    "email": "kmulchrone2l@hc360.com",
+    "carBrand": "Lincoln",
+    "carModel": "MKX",
+    "carYear": 2013
+  }, {
+    "id": 95,
+    "name": "Langsdon Hunn",
+    "email": "lhunn2m@yandex.ru",
+    "carBrand": "Mazda",
+    "carModel": "MX-6",
+    "carYear": 1996
+  }, {
+    "id": 96,
+    "name": "Engelbert Tomczynski",
+    "email": "etomczynski2n@printfriendly.com",
+    "carBrand": "Suzuki",
+    "carModel": "SJ 410",
+    "carYear": 1986
+  }, {
+    "id": 97,
+    "name": "Rene Vaudrey",
+    "email": "rvaudrey2o@freewebs.com",
+    "carBrand": "Nissan",
+    "carModel": "Pathfinder Armada",
+    "carYear": 2004
+  }, {
+    "id": 98,
+    "name": "Terza Sneller",
+    "email": "tsneller2p@unesco.org",
+    "carBrand": "Mitsubishi",
+    "carModel": "Outlander",
+    "carYear": 2004
+  }, {
+    "id": 99,
+    "name": "Ailis Sheeran",
+    "email": "asheeran2q@hc360.com",
+    "carBrand": "Mazda",
+    "carModel": "MX-5",
+    "carYear": 1990
+  }, {
+    "id": 100,
+    "name": "Reeba Rankine",
+    "email": "rrankine2r@fc2.com",
+    "carBrand": "Mitsubishi",
+    "carModel": "Pajero",
+    "carYear": 2003
+  }]
