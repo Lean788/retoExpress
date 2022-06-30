66af38c0d869ec368a2847a22755409cb8122e8c
Leandro S <76525924+Lean788@users.noreply.github.com>
2022-07-01 00:59:37 +0200

    Created: endpoints and MVC dir

--- server.js
+++ server.js
@@ -1,11 +1,48 @@
 const express = require('express');
-require('dotenv').config();
-
-const PORT = process.env.PORT || 3000;
 const app = express();
+require('dotenv').config();
+let db = require('./db.js');
 
+//configure
+app.set('json spaces', 2);
+const PORT = process.env.PORT || 3000;
 
+// Middleware
 app.use(express.json());
+
+app.get('/cars', (req, res) => {
+    res.send(db);
+});
+
+app.get('/cars/:id', (req, res) => {
+    const  { id } = req.params;
+    let car = db.find((car) => car.id == id);
+    res.json(car);
+});
+
+app.post('/cars', (req, res) => {
+    const  {id, name, email, carBrand, carModel, carYear} = req.body;
+    const car = {id, name, email, carBrand, carModel, carYear};
+    db.push(car);
+    res.json(car);
+});
+
+app.put('/cars/:id', (req, res) => {
+    const  { id } = req.params;
+    const { name, email, carBrand, carModel, carYear } = req.body;
+    let carsList = db.filter(car => car.id != id);
+    let car = {id, name, email, carBrand, carModel, carYear};
+    carsList.push(car);
+    db = carsList;
+    res.json(car);
+});
+
+app.delete('/cars/:id', (req, res) => {
+    const  { id } = req.params;
+    let carsList = db.filter((car) => car.id != id);
+    db = carsList;
+    res.json('OK');
+});
 
 app.get('/', (req, res) => {
     res.send('Hello world!')
